f165b5ff55555b1e07af1ad6ceb7aca4625ffe61
PancenOye007 <115642214+PancenOye007@users.noreply.github.com>
2023-02-24 07:47:07 +0700

    Create WH300x250.js

---
+++ WH300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '652e7d7e1176fd9cc38a66880603e520',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/652e7d7e1176fd9cc38a66880603e520/invoke.js"></scr' + 'ipt>');
